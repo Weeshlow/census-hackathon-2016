bea9181ae38846fc22e06ec4e691446b7c76a807
kbirk <birk.kevin@gmail.com>
2016-01-12 10:55:42 -0500

    Added 2hre-tvqe.json parser module.

---
+++ parser/modules/2hre-tvqe.js
@@ -0,0 +1,337 @@
+/*
+    NOTE: ALL POSITION VALUES ARE INCORRECT, THE FIRST 8 ROWS CONTAIN METADATA
+
+    [{
+        "id" : 219993214,
+        "name" : "PermitNumber",
+        "dataTypeName" : "text",
+        "fieldName" : "permitnumber",
+        "position" : 1,
+        "renderTypeName" : "text",
+        "tableColumnId" : 21409609,
+        "width" : 244,
+      }, {
+        "id" : 219993215,
+        "name" : "WORKTYPE",
+        "dataTypeName" : "text",
+        "fieldName" : "worktype",
+        "position" : 2,
+        "renderTypeName" : "text",
+        "tableColumnId" : 21409610,
+        "width" : 196,
+      }, {
+        "id" : 219993216,
+        "name" : "PermitTypeDescr",
+        "dataTypeName" : "text",
+        "fieldName" : "permittypedescr",
+        "position" : 3,
+        "renderTypeName" : "text",
+        "tableColumnId" : 21409611,
+        "width" : 280,
+      }, {
+        "id" : 219993217,
+        "name" : "DESCRIPTION",
+        "dataTypeName" : "text",
+        "fieldName" : "description",
+        "position" : 4,
+        "renderTypeName" : "text",
+        "tableColumnId" : 21409612,
+        "width" : 232,
+      }, {
+        "id" : 219993218,
+        "name" : "Comments",
+        "dataTypeName" : "text",
+        "fieldName" : "comments",
+        "position" : 5,
+        "renderTypeName" : "text",
+        "tableColumnId" : 21409613,
+        "width" : 196,
+      }, {
+        "id" : 219993219,
+        "name" : "ApplicationMethod",
+        "dataTypeName" : "text",
+        "fieldName" : "applicationmethod",
+        "position" : 6,
+        "renderTypeName" : "text",
+        "tableColumnId" : 30793466,
+        "width" : 100,
+      }, {
+        "id" : 219993220,
+        "name" : "APPLICANT",
+        "dataTypeName" : "text",
+        "fieldName" : "applicant",
+        "position" : 7,
+        "renderTypeName" : "text",
+        "tableColumnId" : 21409614,
+        "width" : 208,
+      }, {
+        "id" : 219993221,
+        "name" : "Applicant_Address",
+        "dataTypeName" : "text",
+        "fieldName" : "applicant_address",
+        "position" : 8,
+        "renderTypeName" : "text",
+        "tableColumnId" : 22109209,
+        "width" : 100,
+      }, {
+        "id" : 219993222,
+        "name" : "Applicant_City",
+        "dataTypeName" : "text",
+        "fieldName" : "applicant_city",
+        "position" : 9,
+        "renderTypeName" : "text",
+        "tableColumnId" : 22109210,
+        "width" : 100,
+      }, {
+        "id" : 219993223,
+        "name" : "Applicant_State",
+        "dataTypeName" : "text",
+        "fieldName" : "applicant_state",
+        "position" : 10,
+        "renderTypeName" : "text",
+        "tableColumnId" : 22109646,
+        "width" : 100,
+      }, {
+        "id" : 219993224,
+        "name" : "Applicant_ZIP",
+        "dataTypeName" : "text",
+        "fieldName" : "applicant_zip",
+        "position" : 11,
+        "renderTypeName" : "text",
+        "tableColumnId" : 22109645,
+        "width" : 100,
+      }, {
+        "id" : 219993225,
+        "name" : "Is_Contractor",
+        "dataTypeName" : "html",
+        "fieldName" : "is_contractor",
+        "position" : 12,
+        "renderTypeName" : "html",
+        "tableColumnId" : 22109647,
+        "width" : 100,
+      }, {
+        "id" : 219993226,
+        "name" : "DECLARED_VALUATION",
+        "dataTypeName" : "money",
+        "fieldName" : "declared_valuation",
+        "position" : 13,
+        "renderTypeName" : "money",
+        "tableColumnId" : 21409615,
+        "width" : 316,
+      }, {
+        "id" : 219993227,
+        "name" : "TOTAL_FEES",
+        "dataTypeName" : "money",
+        "fieldName" : "total_fees",
+        "position" : 14,
+        "renderTypeName" : "money",
+        "tableColumnId" : 21409616,
+        "width" : 220,
+      }, {
+        "id" : 219993228,
+        "name" : "ISSUED_DATE",
+        "dataTypeName" : "calendar_date",
+        "fieldName" : "issued_date",
+        "position" : 15,
+        "renderTypeName" : "calendar_date",
+        "tableColumnId" : 21409617,
+        "width" : 232,
+      }, {
+        "id" : 219993229,
+        "name" : "EXPIRATION_DATE",
+        "dataTypeName" : "calendar_date",
+        "fieldName" : "expiration_date",
+        "position" : 16,
+        "renderTypeName" : "calendar_date",
+        "tableColumnId" : 21409618,
+        "width" : 280,
+      }, {
+        "id" : 219993230,
+        "name" : "STATUS",
+        "dataTypeName" : "text",
+        "fieldName" : "status",
+        "position" : 17,
+        "renderTypeName" : "text",
+        "tableColumnId" : 21409619,
+        "width" : 172,
+      }, {
+        "id" : 219993231,
+        "name" : "OWNER",
+        "dataTypeName" : "text",
+        "fieldName" : "owner",
+        "position" : 18,
+        "renderTypeName" : "text",
+        "tableColumnId" : 21409620,
+        "width" : 160,
+      }, {
+        "id" : 219993232,
+        "name" : "OCCUPANCYTYPE",
+        "dataTypeName" : "text",
+        "fieldName" : "occupancytype",
+        "position" : 19,
+        "renderTypeName" : "text",
+        "tableColumnId" : 21409621,
+        "width" : 256,
+      }, {
+        "id" : 219993233,
+        "name" : "sq_feet",
+        "dataTypeName" : "number",
+        "fieldName" : "sq_feet",
+        "position" : 20,
+        "renderTypeName" : "number",
+        "tableColumnId" : 21409622,
+        "width" : 184,
+      }, {
+        "id" : 219993234,
+        "name" : "ADDRESS",
+        "dataTypeName" : "text",
+        "fieldName" : "address",
+        "position" : 21,
+        "renderTypeName" : "text",
+        "tableColumnId" : 21409623,
+        "width" : 184,
+      }, {
+        "id" : 219993235,
+        "name" : "CITY",
+        "dataTypeName" : "text",
+        "fieldName" : "city",
+        "position" : 22,
+        "renderTypeName" : "text",
+        "tableColumnId" : 21409624,
+        "width" : 148,
+      }, {
+        "id" : 219993236,
+        "name" : "STATE",
+        "dataTypeName" : "text",
+        "fieldName" : "state",
+        "position" : 23,
+        "renderTypeName" : "text",
+        "tableColumnId" : 21409625,
+        "width" : 160,
+      }, {
+        "id" : 219993237,
+        "name" : "ZIP",
+        "dataTypeName" : "text",
+        "fieldName" : "zip",
+        "position" : 24,
+        "renderTypeName" : "text",
+        "tableColumnId" : 21409626,
+        "width" : 136,
+      }, {
+        "id" : 219993238,
+        "name" : "Property_ID",
+        "dataTypeName" : "text",
+        "fieldName" : "property_id",
+        "position" : 25,
+        "renderTypeName" : "text",
+        "tableColumnId" : 21409627,
+        "width" : 232,
+      }, {
+        "id" : 219993239,
+        "name" : "Parcel_ID",
+        "dataTypeName" : "text",
+        "fieldName" : "parcel_id",
+        "position" : 26,
+        "renderTypeName" : "text",
+        "tableColumnId" : 21409628,
+        "width" : 208,
+      }, {
+        "id" : 219993240,
+        "name" : "Location",
+        "dataTypeName" : "location",
+        "fieldName" : "location",
+        "position" : 27,
+        "renderTypeName" : "location",
+        "tableColumnId" : 30790052,
+        "width" : 100,
+        "subColumnTypes" : [ "human_address", "latitude", "longitude", "machine_address", "needs_recoding" ]
+      }
+    ]
+*/
+
+/*
+    SAMPLE ROW
+    {
+      0: 461725,
+      1: "FFD8005B-3905-4358-A38E-E8F8FDEE4A0E",
+      2: 461725,
+      3: 1440447167,
+      4: 525314",
+      5: 1440447167,
+      6: "525314",
+      7: null,
+      8: "SF68532",
+      9: "INTREN",
+      10: "Short Form Bldg Permit",
+      11: "Renovations -Interior  NSC",
+      12: "Construct walls to create lobby & office. replace ceilings  install new doors. Install new lights. Install new HVAC. Rework electrical as needed. Install new flooring. Paint.",
+      13: "OnlinePermit",
+      14: "William MacDougall",
+      15: "27 Howland Rd.",
+      16: "Berkley",
+      17: "MA",
+      18: "02779",
+      19: "N",
+      20: "47340",
+      21: "500",
+      22: "2011-03-23T13:32:54",
+      23: "2011-09-23T00:00:00",
+      24: "EXPIRED",
+      25: "BUMC ENERGY MANAGER OFFICE",
+      26: "Mixed",
+      27: "180099",
+      28: "75   E Newton ST",
+      29: "Roxbury",
+      30: "MA",
+      31: "02118",
+      32: "164797",
+      33: "0801405000",
+      34: [ "{\"address\":\"\",\"city\":\"\",\"state\":\"\",\"zip\":\"\"}", "53.4107552408284", "-113.529505586126", null, false ],
+    }
+*/
+
+(function() {
+    'use strict';
+
+    var CENTS_MULTIPLIER = 100;
+    var DATE_COL = 22;
+    var LAT_COL = 34;
+    var LON_COL = 34;
+    var DESC_COL = 11;
+    var AMOUNT_COL = 21;
+    var TYPE_COL = 10;
+    var STATUS_COL = 24;
+
+    module.exports = function(id) {
+        return {
+            id: function() {
+                return id;
+            },
+            type: function(arr) {
+                return arr[TYPE_COL];
+            },
+            status: function(arr) {
+                return arr[STATUS_COL];
+            },
+            location: function(arr) {
+                return {
+                    lat: arr[LAT_COL][1],
+                    lon: arr[LON_COL][2]
+                };
+            },
+            units: function() {
+                return null;
+            },
+            timestamp: function(arr) {
+                return arr[DATE_COL];
+            },
+            description: function(arr) {
+                return arr[DESC_COL];
+            },
+            amount: function(arr) {
+                return arr[AMOUNT_COL] * CENTS_MULTIPLIER;
+            }
+        };
+    };
+
+}());
