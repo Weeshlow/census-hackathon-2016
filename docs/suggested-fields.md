f80601a632a815aff88e9a68f8e53a2c2018aaf9
kbirk <birk.kevin@gmail.com>
2016-01-12 10:21:54 -0500

    Added parse module template and updated suggested fields file.

--- docs/suggested-fields.md
+++ docs/suggested-fields.md
@@ -1,10 +1,31 @@
-# Suggested Fields to pull out
+# Suggested Fields
 
-Lat
-Lon
-Time
-Type
-Status
-Units
-Description
-Amount
+```javascript
+{
+    "Lat": latitude,
+    "Lon": longitude,
+    "Time": time
+    "Type": ?
+    "Status": ?
+    "Units": ?
+    "Amount": dollar amount?
+    "Description": ?
+}
+```
+
+# Suggested Parsed Format
+
+```javascript
+{
+    "location": {
+        "lat": double,
+        "lon": double,
+    },
+    "timestamp": string, // (UTC ISO 8601)
+    "type": string,
+    "status": string,
+    "units": integer,
+    "amount": integer, // (CENTS)
+    "description": string
+}
+```
--- parser/modules/24uj-dj8v.js
+++ parser/modules/24uj-dj8v.js
@@ -1,16 +1,105 @@
+/*
+    NOTE: ALL POSITION VALUES ARE INCORRECT, THE FIRST 8 ROWS CONTAIN METADATA
+
+    *** PUT SCHEMA HEADER HERE ***
+
+    Ex:
+
+    [{
+        "id" : 209261636,
+        "name" : "PERMIT_DATE",
+        "dataTypeName" : "calendar_date",
+        "description" : "The date when Building Permit has been issued.",
+        "fieldName" : "issue_date",
+        "position" : 1,
+        "renderTypeName" : "calendar_date",
+        "tableColumnId" : 20545861,
+        "width" : 127,
+    },{
+        "id" : 209261637,
+        "name" : "PERMIT_NUMBER",
+        "dataTypeName" : "text",
+        "description" : "The unique City-issued permit number ",
+        "fieldName" : "permit_number",
+        "position" : 2,
+        "renderTypeName" : "text",
+        "tableColumnId" : 20545862,
+        "width" : 142,
+    }
+    ...
+    ]
+*/
+
+/*
+    *** PUT A SAMPLE ROW HERE ***
+
+    Ex:
+
+    {
+        0: 342118,
+        1: "40D4B809-2D01-47F2-94FD-2B3FADC6214C",
+        2: 342118,
+        3: 1451733094,
+        4: "393213",
+        5: 1451733094,
+        6: "393213",
+        7: null,
+        8: "2015-12-31T00:00:00",
+        9: "185157437-002",
+        10: "2015",
+        11: "12",
+        12: "2015-12-31T00:00:00",
+        13: "Other Miscellaneous Building",
+        14: "11807 - 21 AVENUE SW",
+        15: "Plan 0620257 Blk 21 Lot 18B",
+        16: "RUTHERFORD",
+        17: "5454",
+        18: "To construct interior alterations to an existing Semi-Detached House, Basement development (NOT to be used as an additional Dwelling).1 bedroom, 1 bathroom, living room, mechanical room, storage,",
+        19: "Semi-Detached House (210)",
+        20: "(03) Interior Alterations",
+        21: "375",
+        22: "15000",
+        23: "RA7, RF4",
+        24: "0",
+        25: "53.4107552408284",
+        26: "-113.529505586126",
+        27: [ "{\"address\":\"\",\"city\":\"\",\"state\":\"\",\"zip\":\"\"}", "53.4107552408284", "-113.529505586126", null, false ],
+        28: "1"
+    }
+*/
+
 (function() {
     'use strict';
 
-    var LAT_COL = 19;
-    var LON_COL = 20;
-
-    module.exports = {
-        location: function (obj) {
-            return {
-                lat: obj[LAT_COL],
-                lon: obj[LON_COL]
-            };
-        }
+    module.exports = function(id) {
+        return {
+            id: function() {
+                return id;
+            },
+            type: function(/* arr */) {
+                return null;
+            },
+            status: function(/* arr */) {
+                return null;
+            },
+            location: function(/* arr */) {
+                return null;
+            },
+            units: function(/* arr */) {
+                return null;
+            },
+            timestamp: function(/* arr */) {
+                // ISO Date format!
+                return null;
+            },
+            description: function() {
+                return null;
+            },
+            amount: function(/* arr */) {
+                // Use Cents!
+                return null;
+            }
+        };
     };
 
 }());
