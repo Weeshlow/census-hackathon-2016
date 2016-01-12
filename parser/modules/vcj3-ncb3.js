ca466583aca94cf3f8eb4b35d4b522fedda0b7b8
Scott Langevin <slangevin@uncharted.software>
2016-01-12 11:56:44 -0500

    added vcj3-ncb3.js

---
+++ parser/modules/vcj3-ncb3.js
@@ -0,0 +1,343 @@
+/*
+    NOTE: ALL POSITION VALUES ARE INCORRECT, THE FIRST 8 ROWS CONTAIN METADATA
+
+    [{
+        "id" : 152247058,
+        "name" : "Number",
+        "dataTypeName" : "number",
+        "fieldName" : "number",
+        "position" : 2,
+        "renderTypeName" : "number",
+        "tableColumnId" : 20204616,
+    },{
+        "id" : 152247059,
+        "name" : "Fraction",
+        "dataTypeName" : "text",
+        "fieldName" : "fraction",
+        "position" : 3,
+        "renderTypeName" : "text",
+        "tableColumnId" : 20204617,
+      }, {
+        "id" : 152247060,
+        "name" : "Prefix",
+        "dataTypeName" : "text",
+        "fieldName" : "prefix",
+        "position" : 4,
+        "renderTypeName" : "text",
+        "tableColumnId" : 20204618,
+      }, {
+        "id" : 152247061,
+        "name" : "Name",
+        "dataTypeName" : "text",
+        "fieldName" : "name",
+        "position" : 5,
+        "renderTypeName" : "text",
+        "tableColumnId" : 20204619,
+      }, {
+        "id" : 152247062,
+        "name" : "Suffix",
+        "dataTypeName" : "text",
+        "fieldName" : "suffix",
+        "position" : 6,
+        "renderTypeName" : "text",
+        "tableColumnId" : 20204620,
+      }, {
+        "id" : 152247063,
+        "name" : "Month",
+        "dataTypeName" : "number",
+        "fieldName" : "month",
+        "position" : 7,
+        "renderTypeName" : "number",
+        "tableColumnId" : 20204621,
+      }, {
+        "id" : 152247064,
+        "name" : "Day",
+        "dataTypeName" : "number",
+        "fieldName" : "day",
+        "position" : 8,
+        "renderTypeName" : "number",
+        "tableColumnId" : 20204622,
+    }, {
+        "id" : 152247065,
+        "name" : "Year",
+        "dataTypeName" : "number",
+        "fieldName" : "year",
+        "position" : 9,
+        "renderTypeName" : "number",
+        "tableColumnId" : 20204623,
+      }, {
+        "id" : 152247066,
+        "name" : "Res_Non",
+        "dataTypeName" : "text",
+        "fieldName" : "res_non",
+        "position" : 10,
+        "renderTypeName" : "text",
+        "tableColumnId" : 20204624,
+      }, {
+        "id" : 152247067,
+        "name" : "Type",
+        "dataTypeName" : "text",
+        "fieldName" : "type",
+        "position" : 11,
+        "renderTypeName" : "text",
+        "tableColumnId" : 20204625,
+      }, {
+        "id" : 152247068,
+        "name" : "SF_MF",
+        "dataTypeName" : "text",
+        "fieldName" : "sf_mf",
+        "position" : 12,
+        "renderTypeName" : "text",
+        "tableColumnId" : 20204626,
+      }, {
+        "id" : 152247069,
+        "name" : "Dwelling Units Gained or Lost",
+        "dataTypeName" : "number",
+        "fieldName" : "dwelling_units_gained_or_lost",
+        "position" : 13,
+        "renderTypeName" : "number",
+        "tableColumnId" : 20204627,
+      }, {
+        "id" : 152247070,
+        "name" : "Permit Value",
+        "dataTypeName" : "number",
+        "fieldName" : "permit_value",
+        "position" : 14,
+        "renderTypeName" : "number",
+        "tableColumnId" : 20204628,
+      }, {
+        "id" : 152247071,
+        "name" : "How Far East",
+        "dataTypeName" : "number",
+        "fieldName" : "how_far_east",
+        "position" : 15,
+        "renderTypeName" : "number",
+        "tableColumnId" : 20204629,
+      }, {
+        "id" : 152247072,
+        "name" : "How Far North",
+        "dataTypeName" : "number",
+        "fieldName" : "how_far_north",
+        "position" : 16,
+        "renderTypeName" : "number",
+        "tableColumnId" : 20204630,
+      }, {
+        "id" : 152247073,
+        "name" : "Permit Number",
+        "dataTypeName" : "number",
+        "fieldName" : "permit_number",
+        "position" : 17,
+        "renderTypeName" : "number",
+        "tableColumnId" : 20204631,
+      }, {
+        "id" : 152247074,
+        "name" : "Structure Class",
+        "dataTypeName" : "number",
+        "fieldName" : "structure_class",
+        "position" : 18,
+        "renderTypeName" : "number",
+        "tableColumnId" : 20204632,
+      }, {
+        "id" : 152247075,
+        "name" : "Class Description",
+        "dataTypeName" : "text",
+        "fieldName" : "class_description",
+        "position" : 19,
+        "renderTypeName" : "text",
+        "tableColumnId" : 20204633,
+      }, {
+        "id" : 152247076,
+        "name" : "Subdivision",
+        "dataTypeName" : "text",
+        "fieldName" : "subdivision",
+        "position" : 20,
+        "renderTypeName" : "text",
+        "tableColumnId" : 20204634,
+      }, {
+        "id" : 152247077,
+        "name" : "Project Description",
+        "dataTypeName" : "text",
+        "fieldName" : "project_description",
+        "position" : 21,
+        "renderTypeName" : "text",
+        "tableColumnId" : 20204635,
+      }, {
+        "id" : 152247078,
+        "name" : "County",
+        "dataTypeName" : "text",
+        "fieldName" : "county",
+        "position" : 22,
+        "renderTypeName" : "text",
+        "tableColumnId" : 20204636,
+      }, {
+        "id" : 152247079,
+        "name" : "Council District",
+        "dataTypeName" : "number",
+        "fieldName" : "council_district",
+        "position" : 23,
+        "renderTypeName" : "number",
+        "tableColumnId" : 20204637,
+      }, {
+        "id" : 152247080,
+        "name" : "School District",
+        "dataTypeName" : "text",
+        "fieldName" : "school_district",
+        "position" : 24,
+        "renderTypeName" : "text",
+        "tableColumnId" : 20204638,
+      }, {
+        "id" : 152247081,
+        "name" : "PIN",
+        "dataTypeName" : "number",
+        "fieldName" : "pin",
+        "position" : 25,
+        "renderTypeName" : "number",
+        "tableColumnId" : 20204639,
+      }, {
+        "id" : 152247082,
+        "name" : "Parcel",
+        "dataTypeName" : "text",
+        "fieldName" : "parcel",
+        "position" : 26,
+        "renderTypeName" : "text",
+        "tableColumnId" : 20204640,
+      }, {
+        "id" : 152247083,
+        "name" : "Applicant Name",
+        "dataTypeName" : "text",
+        "fieldName" : "applicant_name",
+        "position" : 27,
+        "renderTypeName" : "text",
+        "tableColumnId" : 20204641,
+      }, {
+        "id" : 152247084,
+        "name" : "Applicant Representative",
+        "dataTypeName" : "text",
+        "fieldName" : "applicant_representative",
+        "position" : 28,
+        "renderTypeName" : "text",
+        "tableColumnId" : 20204642,
+      }, {
+        "id" : 152247085,
+        "name" : "Owner Name",
+        "dataTypeName" : "text",
+        "fieldName" : "owner_name",
+        "position" : 29,
+        "renderTypeName" : "text",
+        "tableColumnId" : 20204643,
+      }, {
+        "id" : 152247086,
+        "name" : "Legal Description",
+        "dataTypeName" : "text",
+        "fieldName" : "legal_description",
+        "position" : 30,
+        "renderTypeName" : "text",
+        "tableColumnId" : 20204644,
+      }, {
+        "id" : 152247087,
+        "name" : "Location 1",
+        "dataTypeName" : "location",
+        "fieldName" : "location_1",
+        "position" : 31,
+        "renderTypeName" : "location",
+        "tableColumnId" : 20204645,
+      }
+    ]
+*/
+
+/*
+    SAMPLE ROW
+    {
+        0: 1, 
+        1: "FDE86284-3AB0-49D4-851D-1AED5B1416C0", 
+        2: 1, 
+        3: 1402432974, 
+        4: "701271", 
+        5: 1402434307, 
+        6: "701271", 
+        7: "{\n}", 
+        8: "908", 
+        9: null, 
+        10: "NE", 
+        11: "100TH", 
+        12: "CT", 
+        13: "5", 
+        14: "26", 
+        15: "2010", 
+        16: "Residential", 
+        17: "Additions, Alterations, Repairs", 
+        18: "NA", 
+        19: "0", 
+        20: "3630", 
+        21: "2770140", 
+        22: "1131658", 
+        23: "201110970", 
+        24: "97", 
+        25: "Existing One Or Two Family Residence", 
+        26: null, 
+        27: "SINGLE FAMILY", 
+        28: "Clay", 
+        29: "2", 
+        30: "North Kansas City", 
+        31: "103892", 
+        32: "CL0991500030750001", 
+        33: "APEX HEATING AND AIR CONDITIONING INC.", 
+        34: "BRENDA ZITTEL", 
+        35: null, 
+        36: "908 NE 100TH CT               WILLOW PARK 2ND PLAT LOT 50", 
+        37: [ "{\"address\":\"908 NE 100TH CT\",\"city\":\"\",\"state\":\"\",\"zip\":\"\"}", "39.52368635500045", "-99.11415045099966", null, false ] ]
+    }
+*/
+
+(function() {
+    'use strict';
+
+    var CENTS_MULTIPLIER = 100;
+    var YEAR_COL = 15;
+    var MONTH_COL = 13;
+    var DAY_COL = 14;
+
+    var LOC_COL = 37;
+    var LOC_LAT_COL = 1;
+    var LOC_LON_COL = 2;
+
+    var CLASS_COL = 16;
+    var TYPE_COL = 17;
+
+    var UNITS_COL = 19;
+    var DESC_COL = 25;
+    var AMOUNT_COL = 20;
+
+    module.exports = function(id) {
+        return {
+            id: function() {
+                return id;
+            },
+            type: function() {
+                return arr[TYPE_COL];
+            },
+            status: function() {
+                return null;
+            },
+            latitude: function(arr) {
+                return arr[LOC_COL][LOC_LAT_COL];
+            },
+            longitude: function(arr) {
+                return arr[LOC_COL][LOC_LON_COL];
+            },
+            units: function(arr) {
+                return arr[UNITS_COL];
+            },
+            timestamp: function(arr) {
+                return arr[YEAR_COL] + '-' + arr[MONTH_COL] + '-' + arr[DAY_COL] + 'T00:00:00';  // ISO datetime string
+            },
+            description: function(arr) {
+                return arr[TYPE_COL] + ' - ' + arr[DESC_COL];
+            },
+            amount: function(arr) {
+                return arr[AMOUNT_COL] * CENTS_MULTIPLIER;
+            }
+        };
+    };
+
+}());
