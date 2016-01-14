182dc07678e90fd1dabfa9937d2cc4366276f862
Sean McIntyre <s.mcintyre@xverba.ca>
2016-01-14 18:34:45 -0500

    Implemented job for permits by culure layers

---
+++ census-salt/generation/src/main/scala/software/uncharted/censushackathon2016/jobs/MinorityVsNonMinorityJob.scala
@@ -0,0 +1,51 @@
+package software.uncharted.censushackathon2016.jobs
+
+import org.apache.spark.sql.{DataFrame, SQLContext, Row}
+
+import software.uncharted.sparkpipe.Pipe
+import software.uncharted.sparkpipe.ops
+
+import software.uncharted.censushackathon2016.{layers, OutputterFactory}
+
+import software.uncharted.salt.core.generation.request.TileLevelRequest
+import software.uncharted.salt.core.generation.mapreduce.MapReduceTileGenerator
+
+object MinorityVsNonMinorityJob {
+  def run(data: DataFrame, levelBatches: Seq[Seq[Int]], outputterFactory: OutputterFactory) = {
+    // Construct an RDD of Rows containing only the fields we need. Cache the result
+    val input = Pipe(data)
+      .to(_.select("longitude", "latitude", "applicantRace", "applicantRaceProb"))
+      .to(ops.core.dataframe.cache)
+      .to(ops.core.dataframe.toRDD)
+      .to(sourceData => {
+
+        // Tile Generator object, which houses the generation logic
+        val gen = new MapReduceTileGenerator(data.sqlContext.sparkContext)
+
+        // Iterate over sets of levels to generate.
+        Pipe(levelBatches)
+        .to(_.map(level => {
+
+          println("------------------------------")
+          println(s"Generating level $level")
+          println("------------------------------")
+
+          val nonMinority = new layers.CultureHeatMapLayer("non-minority-permits", level, (c: String) => c == "white")
+          val minorities = new layers.CultureHeatMapLayer("minority-permits", level, (c: String) => c != "white")
+
+          // Create a request for all tiles on these levels, generate
+          val request = new TileLevelRequest(level, (coord: (Int, Int, Int)) => coord._1)
+          val tiles = gen.generate(
+            sourceData,
+            Seq(nonMinority.series, minorities.series),
+            request
+          )
+          nonMinority.serialize(level, tiles, outputterFactory)
+          minorities.serialize(level, tiles, outputterFactory)
+        }))
+        .run;
+
+      })
+      .run;
+  }
+}
---
+++ census-salt/generation/src/main/scala/software/uncharted/censushackathon2016/layers/CultureHeatMapLayer.scala
@@ -0,0 +1,73 @@
+package software.uncharted.censushackathon2016.layers
+
+import org.apache.spark.SparkContext
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.{Row, DataFrame}
+
+import software.uncharted.censushackathon2016.{OutputterFactory,ByteBufferCreator}
+
+import software.uncharted.sparkpipe.Pipe
+import software.uncharted.sparkpipe.ops
+
+
+import software.uncharted.salt.core.projection.numeric.MercatorProjection
+import software.uncharted.salt.core.generation.Series
+import software.uncharted.salt.core.generation.output.{Tile, SeriesData}
+import software.uncharted.salt.core.analytic.numeric.{SumAggregator, MinMaxAggregator}
+
+class CultureHeatMapLayer(layerName: String, levels: Seq[Int], cultureFilter: (String) => Boolean) extends Serializable {
+  private val tileSize = 256;
+
+  // Given an input row, return permit longitude, latitude as a tuple
+  private val permitExtractor = (r: Row) => {
+    if (r.isNullAt(0) || r.isNullAt(1) || r.isNullAt(2) || r.isNullAt(3) || !cultureFilter(r.getString(2))) {
+      None
+    } else {
+      Some((r.getDouble(0), r.getDouble(1)))
+    }
+  }
+
+  private val amountExtractor = (r: Row) => {
+    if (r.isNullAt(3)) {
+      None
+    } else {
+      Some(r.getDouble(3))
+    }
+  }
+
+  val series = new Series((tileSize - 1, tileSize - 1),
+                          permitExtractor,
+                          new MercatorProjection(levels),
+                          Some(amountExtractor),
+                          SumAggregator,
+                          Some(MinMaxAggregator))
+
+  // extract our SeriesData from each tile and write it out
+  def serialize(level: Seq[Int], tiles: RDD[Tile[(Int, Int, Int)]], outputterFactory: OutputterFactory) = {
+    val seriesData = Pipe(tiles)
+                     .to(_.map(series(_)))
+
+    seriesData
+    .to(_.map(tile => {
+      // Return tuples of tile coordinate, byte array
+      (tile.coords, ByteBufferCreator.create(tile, tileSize*tileSize))
+    }))
+    // .to(_.collect())
+    .to(_.foreachPartition(p => {
+      val outputter = outputterFactory.get()
+      p.foreach(binTile => {
+        // Save byte files to local filesystem
+        val coord = binTile._1
+        val byteArray = binTile._2
+        val limit = (1 << coord._1) - 1
+
+        outputter.output(
+          s"$layerName/${coord._1}/${coord._2}/${limit - coord._3}.bin", //TMS style
+          "application/json",
+          byteArray
+        )
+      })
+    }))
+    .run
+  }
+}
